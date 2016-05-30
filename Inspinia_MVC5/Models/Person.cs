d677c6a23a6447b02c35372924bbb5061217d381
olgazd <yaroshenko.olga@gmail.com>
2016-05-30 15:48:48 +1200

    Nav bar, top navbar, layout, dashboard2, profile, Social feed modified

---
+++ Inspinia_MVC5/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inspinia_MVC5.Models
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmergencyPhoneNumber { get; set; }
+
+        public string EmergencyContact { get; set; }
+
+        public string Info { get; set; }
+
+        public string Role { get; set; }
+    }
+}
